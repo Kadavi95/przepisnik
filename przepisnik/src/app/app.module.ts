bdf1ffb341f6f2fbb320431dd6009174e14ea0e9
Damian Brzezinski <damian.brzezinski@ppuc.pl>
2022-03-30 04:37:16 +0200

    main configuration. Json and fontAwesome installed

--- przepisnik/src/app/app.module.ts
+++ przepisnik/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+// import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+// import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
+// import { faStar as fasStar } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [
@@ -10,9 +15,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  // constructor(public library: FaIconLibrary){
+  //   library.addIcons(fasStar, farStar);
+  // }
+}
